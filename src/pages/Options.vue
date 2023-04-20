165b3693fac3d154336c0d1fb3c3c65a0e4664ba
CeraceLee <leecerace@outlook.com>
2023-04-20 18:32:39 +0800

    Update Options.vue

--- src/pages/Options.vue
+++ src/pages/Options.vue
@@ -45,13 +45,6 @@
                 <span style="text-align: center;">{{ $t('description') }}</span>
                 <a>v{{ packageInfo.version }}</a>
                 <a class="ss-button" @click="util.openLink('https://github.com/Stapxs/Stapxs-QQ-Lite-2.0')">{{ $t('option_info_visit_github') }}</a>
-                <div v-if="constList.length > 0" class="ss-card contributors-card">
-                    <div>
-                        <div v-for="info in constList" :key="'contributors-' + info.title" :title="info.title"
-                            :style="'background-image: url(' + info.url + ');'" :class="info.isMe ? 'me' : ''"
-                            @click="util.openLink(info.link)"></div>
-                    </div>
-                </div>
             </div>
             <div class="ss-card bcd-about" @click="util.openLink('https://stapxs.github.io/Border-Card-UI/docs/')">
                 <div>
@@ -213,8 +206,6 @@ import OptView from './options/OptView.vue'
 import OptDev from './options/OptDev.vue'
 import OptFunction from './options/OptFunction.vue'
 
-import { ContributorElem } from '@/function/elements/system'
-
 export default defineComponent({
     name: 'ViewOption',
     props: {
@@ -223,26 +214,11 @@ export default defineComponent({
     components: { OptAccount, OptView, OptDev, OptFunction },
     data() {
         return {
-            constList: [] as ContributorElem[],
             packageInfo: packageInfo,
             util: Util
         }
     },
     mounted() {
-        // 加载贡献者信息
-        fetch('https://api.github.com/repos/stapxs/stapxs-qq-lite-2.0/contributors')
-            .then(response => response.json())
-            .then((data: { [key: string]: string }[]) => {
-                for (let i = 0; i < data.length; i++) {
-                    this.constList.push({
-                        url: data[i].avatar_url,
-                        link: data[i].html_url,
-                        title: data[i].login,
-                        isMe: data[i].login == 'Stapxs'
-                    })
-                }
-            })
-            .catch(console.error)
     }
 })
 </script>
